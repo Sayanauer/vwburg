7b8aa5e240e5891a3abce3d7f2c8c3c9d88d839e
Lucas <lucas.acdc92@gmail.com>
2022-04-15 23:05:45 -0300

    Precios de abril actualizados

--- public/slider.js
+++ public/slider.js
@@ -1,48 +1,46 @@
-const slider =  document.querySelector('#slider');
+const slider = document.querySelector("#slider");
 
-let sliderSection = document.querySelectorAll('.slider-section');
-let sliderSectionLast = sliderSection[sliderSection.length-1];
+let sliderSection = document.querySelectorAll(".slider-section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
 
-const btnLeft = document.querySelector('#btn-left');
-const btnRight = document.querySelector('#btn-right');
+//const btnLeft = document.querySelector('#btn-left');
+//const btnRight = document.querySelector('#btn-right');
 
-const myslide = document.querySelectorAll('.myslide')//,
-	//  dot = document.querySelectorAll('.dot');
+const myslide = document.querySelectorAll(".myslide"); //,
+//  dot = document.querySelectorAll('.dot');
 let counter = 1;
 slidefun(counter);
 
 let timer = setInterval(autoSlide, 8000);
 function autoSlide() {
-	counter += 1;
-	slidefun(counter);
+  counter += 1;
+  slidefun(counter);
 }
 function plusSlides(n) {
-	counter += n;
-	slidefun(counter);
-	resetTimer();
+  counter += n;
+  slidefun(counter);
+  resetTimer();
 }
 function currentSlide(n) {
-	counter = n;
-	slidefun(counter);
-	resetTimer();
+  counter = n;
+  slidefun(counter);
+  resetTimer();
 }
 function resetTimer() {
-	clearInterval(timer);
-	timer = setInterval(autoSlide, 5000);
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 5000);
 }
 
 function slidefun(n) {
-	
-	let i;
-	for(i = 0;i<myslide.length;i++){
-		myslide[i].style.display = "none";
-	}
-	if(n > myslide.length){
-	   counter = 1;
-	   }
-	if(n < 1){
-	   counter = myslide.length;
-	   }
-	myslide[counter - 1].style.display = "block";
-	//dot[counter - 1].className += " active";
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    myslide[i].style.display = "none";
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
 }
